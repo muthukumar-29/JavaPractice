7c56517174dcf7780ad48251aff063bbaffe1a22
Muthukumar M <muthukumarm.2903@gmail.com>
2024-01-21 18:28:38 +0000

    First Commit

--- HighandLow.java
+++ HighandLow.java
@@ -1,9 +1,12 @@
+import java.util.*;
 public class HighandLow {
 
-    public static void highOrLow(){
+    public static void highOrLow(String num){
 
     }
     public static void main(String args[]){
+        String n;
+        Scanner s=new Scanner(System.in);
         
     }
 }
